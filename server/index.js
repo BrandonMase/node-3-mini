d51f35df091b38b1095863dfee0980fcf90c871e
Brandon Mase <brandonofsid@gmail.com>
2018-02-13 13:33:15 -0700

    finished mini

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
-
+const session = require('express-session');
+require('dotenv').config();
 const app = express();
+const { createInitialSession } = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        age: 10000,
+    }
+}));
+
+app.use(createInitialSession);
 
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
+app.post( messagesBaseUrl,filter, mc.create );
 app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
-app.delete( `${messagesBaseUrl}`, mc.delete );
+app.put( `${messagesBaseUrl}`,filter, mc.update );
+app.delete(`${messagesBaseUrl}`, mc.delete);
+app.get(`${messagesBaseUrl}/history`, mc.history);
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
